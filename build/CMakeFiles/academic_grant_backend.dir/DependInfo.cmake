e4bf37c5ed8d773ad487d75c48f1396c5410d6af
Artem-Tarapovsky <temi4ka@outlook.com>
2025-11-15 01:44:05 +0700

    test 11

---
+++ build/CMakeFiles/academic_grant_backend.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/artem_tarapovsky/Desktop/academic_grant2/src/core/ScholarshipCalculator.cpp" "CMakeFiles/academic_grant_backend.dir/src/core/ScholarshipCalculator.cpp.o" "gcc" "CMakeFiles/academic_grant_backend.dir/src/core/ScholarshipCalculator.cpp.o.d"
+  "/Users/artem_tarapovsky/Desktop/academic_grant2/src/database/DataBaseManager.cpp" "CMakeFiles/academic_grant_backend.dir/src/database/DataBaseManager.cpp.o" "gcc" "CMakeFiles/academic_grant_backend.dir/src/database/DataBaseManager.cpp.o.d"
+  "/Users/artem_tarapovsky/Desktop/academic_grant2/src/database/Models.cpp" "CMakeFiles/academic_grant_backend.dir/src/database/Models.cpp.o" "gcc" "CMakeFiles/academic_grant_backend.dir/src/database/Models.cpp.o.d"
+  "/Users/artem_tarapovsky/Desktop/academic_grant2/src/main.cpp" "CMakeFiles/academic_grant_backend.dir/src/main.cpp.o" "gcc" "CMakeFiles/academic_grant_backend.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/academic_grant_backend.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for academic_grant_backend.
+# This may be replaced when dependencies are built.
